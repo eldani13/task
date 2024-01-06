7dc7525869826e3e6b5ccfacdc88dfac663797e8
JuliannnR <juliroblesfonta07@gmail.com>
2024-01-06 14:06:39 -0500

    ejemplo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react"
+import React, { useState } from "react" 
 export default function Home() {
 
   const [tasks, setTasks] = useState<string[]>([]);
